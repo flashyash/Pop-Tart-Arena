68bcdad20d51716e116f87fa686b9bd053355563
Vilhelm824 <willhfischer@icloud.com>
2024-03-26 15:39:25 -0400

    Local 2 player movement

--- PopTartArena/Assets/Scripts/PlayerMoveAround.cs
+++ PopTartArena/Assets/Scripts/PlayerMoveAround.cs
@@ -2,46 +2,66 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class PlayerMoveAround : MonoBehaviour {
+public class MultiPlayerMoveAround : MonoBehaviour
+{
 
       //public Animator anim;
       //public AudioSource WalkSFX;
       public Rigidbody2D rb2D;
-      private bool FaceRight = true; // determine which way player is facing.
+      private bool FaceRight = false; // determine which way player is facing.
       public static float runSpeed = 10f;
       public float startSpeed = 10f;
       public bool isAlive = true;
+      public bool isPlayer1 = false;
 
-      void Start(){
-           //anim = gameObject.GetComponentInChildren<Animator>();
-           rb2D = transform.GetComponent<Rigidbody2D>();
+      private Vector3 hvMove;
+
+      void Start()
+      {
+            //anim = gameObject.GetComponentInChildren<Animator>();
+            rb2D = transform.GetComponent<Rigidbody2D>();
       }
 
-      void Update(){
+      void Update()
+      {
             //NOTE: Horizontal axis: [a] / left arrow is -1, [d] / right arrow is 1
             //NOTE: Vertical axis: [w] / up arrow, [s] / down arrow
-            Vector3 hvMove = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-           if (isAlive == true){
+            if (isPlayer1 == true)
+            {
+                  hvMove = new Vector3(Input.GetAxis("p1Horiz"), Input.GetAxis("p1Vert"), 0.0f);
+            }
+            else
+            {
+                  hvMove = new Vector3(Input.GetAxis("p2Horiz"), Input.GetAxis("p2Vert"), 0.0f);
+            }
+
+            if (isAlive == true)
+            {
                   transform.position = transform.position + hvMove * runSpeed * Time.deltaTime;
 
-                  if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0)){
-                  //     anim.SetBool ("Walk", true);
-                  //     if (!WalkSFX.isPlaying){
-                  //           WalkSFX.Play();
-                  //     }
-                  } else {
-                  //     anim.SetBool ("Walk", false);
-                  //     WalkSFX.Stop();
-                 }
+                  if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0))
+                  {
+                        //     anim.SetBool ("Walk", true);
+                        //     if (!WalkSFX.isPlaying){
+                        //           WalkSFX.Play();
+                        //     }
+                  }
+                  else
+                  {
+                        //     anim.SetBool ("Walk", false);
+                        //     WalkSFX.Stop();
+                  }
 
                   // Turning. Reverse if input is moving the Player right and Player faces left.
-                 if ((hvMove.x <0 && !FaceRight) || (hvMove.x >0 && FaceRight)){
+                  if ((hvMove.x < 0 && !FaceRight) || (hvMove.x > 0 && FaceRight))
+                  {
                         playerTurn();
                   }
             }
       }
 
-      private void playerTurn(){
+      private void playerTurn()
+      {
             // NOTE: Switch player facing label
             FaceRight = !FaceRight;
 
@@ -50,4 +70,5 @@ public class PlayerMoveAround : MonoBehaviour {
             theScale.x *= -1;
             transform.localScale = theScale;
       }
+
 }
