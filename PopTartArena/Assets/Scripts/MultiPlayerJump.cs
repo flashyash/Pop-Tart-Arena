85a62fd5890ef2a0f11d49ef6215eb7a287a9958
20nikhitak <nikhita1911@gmail.com>
2024-04-09 02:40:54 -0400

    add colliders + movement

--- PopTartArena/Assets/Scripts/MultiPlayerJump.cs
+++ PopTartArena/Assets/Scripts/MultiPlayerJump.cs
@@ -17,6 +17,8 @@ public class PlayerJump : MonoBehaviour
     public bool isPlayer2 = false;
     public bool isPlayer3 = false;
     public bool isPlayer4 = false;
+    public GameObject platforms;
+
     //public AudioSource JumpSFX;
 
     void Start()
@@ -27,7 +29,7 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
-        //if (isPlayer1 == true){
+
         if ((IsGrounded()) || (jumpTimes <= 1))
         {
             // if ((IsGrounded()) && (jumpTimes <= 1)){ // for single jump only
@@ -41,19 +43,92 @@ public class PlayerJump : MonoBehaviour
 
         if ((Input.GetButtonDown("Jump")) && canJump && isAlive)
         {
-            Jump();
+            Jump(Vector2.up);
+        } 
+/*        
+        jump up
+        if (isPlayer1 && Input.GetAxis("p1Vert") > 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.up);
         }
+        if (isPlayer2 && Input.GetAxis("p2Vert") > 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.up);
+        }
+        if (isPlayer3 && Input.GetAxis("p3Vert") > 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.up);
+        }
+        if (isPlayer4 && Input.GetAxis("p4Vert") > 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.up);
+        } 
+
+        // jump down
+        
+        if (isPlayer1 && Input.GetAxis("p1Vert") < 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.down);
+        }
+        if (isPlayer2 && Input.GetAxis("p2Vert") < 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.down);
+        }
+        if (isPlayer3 && Input.GetAxis("p3Vert") < 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.down);
+        }
+        if (isPlayer4 && Input.GetAxis("p4Vert") < 0 && canJump && isAlive) 
+        {
+            Jump(Vector2.down);
+        }
+        
+        */
     }
 
-    public void Jump()
+    public void Jump(Vector2 direction)
     {
         jumpTimes += 1;
-        rb.velocity = Vector2.up * jumpForce;
+        rb.velocity = direction * jumpForce;
         // anim.SetTrigger("Jump");
         // JumpSFX.Play();
 
         //Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
         //rb.velocity = movement;
+
+        // Iterate over all child game objects of the parent
+        foreach (Transform child in platforms.transform)
+        {
+            // Check if the child has a collider component
+            Collider2D collider = child.GetComponent<Collider2D>();
+            if (collider != null)
+            {
+                // Set the collider to be a trigger
+                collider.isTrigger = true;
+            }
+        }
+
+        // Start coroutine to re-enable collider after a short delay
+        StartCoroutine(EnableColliderAfterDelay());
+    }
+
+    private IEnumerator EnableColliderAfterDelay()
+    {
+        // Wait for a short duration (adjust as needed)
+        yield return new WaitForSeconds(0.2f);
+
+        // Re-enable collider's trigger
+        // Iterate over all child game objects of the parent
+        foreach (Transform child in platforms.transform)
+        {
+            // Check if the child has a collider component
+            Collider2D collider = child.GetComponent<Collider2D>();
+            if (collider != null)
+            {
+                // Set the collider to be a trigger
+                collider.isTrigger = false;
+            }
+        }
     }
 
     public bool IsGrounded()
